8fc4198f44337e2e939db4cdf7546c195cee1007
Mai Thanh Minh <thanhminh.mr@gmail.com>
2023-04-21 00:21:23 +0700

    make arithmetic coding part 32 bit instead of 64 bit

--- src/main.rs
+++ src/main.rs
@@ -66,37 +66,37 @@ impl<E: Error> From<E> for AnyError {
 
 // MULTIPLIER[i] == 0x1_0000_0000 / (i + 2)
 const MULTIPLIER: [u32; 256] = [
-	0x80000000, 0x55555555, 0x40000000, 0x33333333, 0x2AAAAAAA, 0x24924924, 0x20000000, 0x1C71C71C,
-	0x19999999, 0x1745D174, 0x15555555, 0x13B13B13, 0x12492492, 0x11111111, 0x10000000, 0x0F0F0F0F,
-	0x0E38E38E, 0x0D79435E, 0x0CCCCCCC, 0x0C30C30C, 0x0BA2E8BA, 0x0B21642C, 0x0AAAAAAA, 0x0A3D70A3,
-	0x09D89D89, 0x097B425E, 0x09249249, 0x08D3DCB0, 0x08888888, 0x08421084, 0x08000000, 0x07C1F07C,
-	0x07878787, 0x07507507, 0x071C71C7, 0x06EB3E45, 0x06BCA1AF, 0x06906906, 0x06666666, 0x063E7063,
-	0x06186186, 0x05F417D0, 0x05D1745D, 0x05B05B05, 0x0590B216, 0x0572620A, 0x05555555, 0x05397829,
-	0x051EB851, 0x05050505, 0x04EC4EC4, 0x04D4873E, 0x04BDA12F, 0x04A7904A, 0x04924924, 0x047DC11F,
-	0x0469EE58, 0x0456C797, 0x04444444, 0x04325C53, 0x04210842, 0x04104104, 0x04000000, 0x03F03F03,
-	0x03E0F83E, 0x03D22635, 0x03C3C3C3, 0x03B5CC0E, 0x03A83A83, 0x039B0AD1, 0x038E38E3, 0x0381C0E0,
-	0x03759F22, 0x0369D036, 0x035E50D7, 0x03531DEC, 0x03483483, 0x033D91D2, 0x03333333, 0x0329161F,
-	0x031F3831, 0x03159721, 0x030C30C3, 0x03030303, 0x02FA0BE8, 0x02F14990, 0x02E8BA2E, 0x02E05C0B,
-	0x02D82D82, 0x02D02D02, 0x02C8590B, 0x02C0B02C, 0x02B93105, 0x02B1DA46, 0x02AAAAAA, 0x02A3A0FD,
-	0x029CBC14, 0x0295FAD4, 0x028F5C28, 0x0288DF0C, 0x02828282, 0x027C4597, 0x02762762, 0x02702702,
-	0x026A439F, 0x02647C69, 0x025ED097, 0x02593F69, 0x0253C825, 0x024E6A17, 0x02492492, 0x0243F6F0,
-	0x023EE08F, 0x0239E0D5, 0x0234F72C, 0x02302302, 0x022B63CB, 0x0226B902, 0x02222222, 0x021D9EAD,
-	0x02192E29, 0x0214D021, 0x02108421, 0x020C49BA, 0x02082082, 0x02040810, 0x02000000, 0x01FC07F0,
-	0x01F81F81, 0x01F44659, 0x01F07C1F, 0x01ECC07B, 0x01E9131A, 0x01E573AC, 0x01E1E1E1, 0x01DE5D6E,
-	0x01DAE607, 0x01D77B65, 0x01D41D41, 0x01D0CB58, 0x01CD8568, 0x01CA4B30, 0x01C71C71, 0x01C3F8F0,
-	0x01C0E070, 0x01BDD2B8, 0x01BACF91, 0x01B7D6C3, 0x01B4E81B, 0x01B20364, 0x01AF286B, 0x01AC5701,
-	0x01A98EF6, 0x01A6D01A, 0x01A41A41, 0x01A16D3F, 0x019EC8E9, 0x019C2D14, 0x01999999, 0x01970E4F,
-	0x01948B0F, 0x01920FB4, 0x018F9C18, 0x018D3018, 0x018ACB90, 0x01886E5F, 0x01861861, 0x0183C977,
-	0x01818181, 0x017F405F, 0x017D05F4, 0x017AD220, 0x0178A4C8, 0x01767DCE, 0x01745D17, 0x01724287,
-	0x01702E05, 0x016E1F76, 0x016C16C1, 0x016A13CD, 0x01681681, 0x01661EC6, 0x01642C85, 0x01623FA7,
-	0x01605816, 0x015E75BB, 0x015C9882, 0x015AC056, 0x0158ED23, 0x01571ED3, 0x01555555, 0x01539094,
-	0x0151D07E, 0x01501501, 0x014E5E0A, 0x014CAB88, 0x014AFD6A, 0x0149539E, 0x0147AE14, 0x01460CBC,
-	0x01446F86, 0x0142D662, 0x01414141, 0x013FB013, 0x013E22CB, 0x013C995A, 0x013B13B1, 0x013991C2,
-	0x01381381, 0x013698DF, 0x013521CF, 0x0133AE45, 0x01323E34, 0x0130D190, 0x012F684B, 0x012E025C,
-	0x012C9FB4, 0x012B404A, 0x0129E412, 0x01288B01, 0x0127350B, 0x0125E227, 0x01249249, 0x01234567,
-	0x0121FB78, 0x0120B470, 0x011F7047, 0x011E2EF3, 0x011CF06A, 0x011BB4A4, 0x011A7B96, 0x01194538,
-	0x01181181, 0x0116E068, 0x0115B1E5, 0x011485F0, 0x01135C81, 0x0112358E, 0x01111111, 0x010FEF01,
-	0x010ECF56, 0x010DB20A, 0x010C9714, 0x010B7E6E, 0x010A6810, 0x010953F3, 0x01084210, 0x01073260,
+	0x80000000, 0x55555555, 0x40000000, 0x33333333, 0x2AAAAAAB, 0x24924925, 0x20000000, 0x1C71C71C,
+	0x1999999A, 0x1745D174, 0x15555555, 0x13B13B14, 0x12492492, 0x11111111, 0x10000000, 0x0F0F0F0F,
+	0x0E38E38E, 0x0D79435E, 0x0CCCCCCD, 0x0C30C30C, 0x0BA2E8BA, 0x0B21642D, 0x0AAAAAAB, 0x0A3D70A4,
+	0x09D89D8A, 0x097B425F, 0x09249249, 0x08D3DCB1, 0x08888889, 0x08421084, 0x08000000, 0x07C1F07C,
+	0x07878788, 0x07507507, 0x071C71C7, 0x06EB3E45, 0x06BCA1AF, 0x06906907, 0x06666666, 0x063E7064,
+	0x06186186, 0x05F417D0, 0x05D1745D, 0x05B05B06, 0x0590B216, 0x0572620B, 0x05555555, 0x0539782A,
+	0x051EB852, 0x05050505, 0x04EC4EC5, 0x04D4873F, 0x04BDA12F, 0x04A7904A, 0x04924925, 0x047DC11F,
+	0x0469EE58, 0x0456C798, 0x04444444, 0x04325C54, 0x04210842, 0x04104104, 0x04000000, 0x03F03F04,
+	0x03E0F83E, 0x03D22635, 0x03C3C3C4, 0x03B5CC0F, 0x03A83A84, 0x039B0AD1, 0x038E38E4, 0x0381C0E0,
+	0x03759F23, 0x0369D037, 0x035E50D8, 0x03531DEC, 0x03483483, 0x033D91D3, 0x03333333, 0x03291620,
+	0x031F3832, 0x03159722, 0x030C30C3, 0x03030303, 0x02FA0BE8, 0x02F14990, 0x02E8BA2F, 0x02E05C0C,
+	0x02D82D83, 0x02D02D03, 0x02C8590B, 0x02C0B02C, 0x02B93105, 0x02B1DA46, 0x02AAAAAB, 0x02A3A0FD,
+	0x029CBC15, 0x0295FAD4, 0x028F5C29, 0x0288DF0D, 0x02828283, 0x027C4598, 0x02762762, 0x02702702,
+	0x026A439F, 0x02647C69, 0x025ED098, 0x02593F6A, 0x0253C825, 0x024E6A17, 0x02492492, 0x0243F6F0,
+	0x023EE090, 0x0239E0D6, 0x0234F72C, 0x02302302, 0x022B63CC, 0x0226B902, 0x02222222, 0x021D9EAD,
+	0x02192E2A, 0x0214D021, 0x02108421, 0x020C49BA, 0x02082082, 0x02040810, 0x02000000, 0x01FC07F0,
+	0x01F81F82, 0x01F4465A, 0x01F07C1F, 0x01ECC07B, 0x01E9131B, 0x01E573AD, 0x01E1E1E2, 0x01DE5D6E,
+	0x01DAE607, 0x01D77B65, 0x01D41D42, 0x01D0CB59, 0x01CD8569, 0x01CA4B30, 0x01C71C72, 0x01C3F8F0,
+	0x01C0E070, 0x01BDD2B9, 0x01BACF91, 0x01B7D6C4, 0x01B4E81B, 0x01B20364, 0x01AF286C, 0x01AC5702,
+	0x01A98EF6, 0x01A6D01A, 0x01A41A42, 0x01A16D40, 0x019EC8E9, 0x019C2D15, 0x0199999A, 0x01970E50,
+	0x01948B10, 0x01920FB5, 0x018F9C19, 0x018D3019, 0x018ACB91, 0x01886E5F, 0x01861862, 0x0183C978,
+	0x01818182, 0x017F4060, 0x017D05F4, 0x017AD221, 0x0178A4C8, 0x01767DCE, 0x01745D17, 0x01724288,
+	0x01702E06, 0x016E1F77, 0x016C16C1, 0x016A13CD, 0x01681681, 0x01661EC7, 0x01642C86, 0x01623FA7,
+	0x01605816, 0x015E75BC, 0x015C9883, 0x015AC057, 0x0158ED23, 0x01571ED4, 0x01555555, 0x01539095,
+	0x0151D07F, 0x01501501, 0x014E5E0A, 0x014CAB88, 0x014AFD6A, 0x0149539E, 0x0147AE14, 0x01460CBC,
+	0x01446F86, 0x0142D662, 0x01414141, 0x013FB014, 0x013E22CC, 0x013C995A, 0x013B13B1, 0x013991C3,
+	0x01381381, 0x013698DF, 0x013521D0, 0x0133AE46, 0x01323E35, 0x0130D190, 0x012F684C, 0x012E025C,
+	0x012C9FB5, 0x012B404B, 0x0129E413, 0x01288B01, 0x0127350C, 0x0125E227, 0x01249249, 0x01234568,
+	0x0121FB78, 0x0120B471, 0x011F7048, 0x011E2EF4, 0x011CF06B, 0x011BB4A4, 0x011A7B96, 0x01194538,
+	0x01181181, 0x0116E069, 0x0115B1E6, 0x011485F1, 0x01135C81, 0x0112358E, 0x01111111, 0x010FEF01,
+	0x010ECF57, 0x010DB20B, 0x010C9715, 0x010B7E6F, 0x010A6811, 0x010953F4, 0x01084211, 0x01073261,
 	0x010624DD, 0x0105197F, 0x01041041, 0x0103091B, 0x01020408, 0x01010101, 0x01000000, 0x00FF00FF,
 ];
 
@@ -107,26 +107,26 @@ struct BitPrediction(u32);
 impl BitPrediction {
 	fn new() -> Self { Self(0x80000000) }
 
-	fn get_prediction(&self) -> u64 { (self.0 >> 8) as u64 }
+	fn get_prediction(&self) -> u32 { self.0 & 0xFFFFFF00 }
 
 	// return current prediction and then update the prediction with new bit
-	fn update(&mut self, bit: usize) -> u64 {
-		assert!(bit == 0 || bit == 1);
+	fn update(&mut self, bit: usize) -> u32 {
+		debug_assert!(bit == 0 || bit == 1);
 		// get bit 0-7 as count
 		let count: usize = (self.0 & 0xFF) as usize;
-		// get bit 8-31 as current prediction
-		let current_prediction: i64 = (self.0 >> 8) as i64;
+		// masking bit 8-31 as old prediction
+		let old_prediction: u32 = self.0 & 0xFFFFFF00;
 		// create bit shift
-		let bit_shift: i64 = (bit as i64) << 24;
+		let bit_shift: i64 = (bit as i64) << 32;
 		// get multiplier
 		let multiplier: i64 = MULTIPLIER[count] as i64;
 		// calculate new prediction
-		let new_prediction: u32 = (((bit_shift - current_prediction) * multiplier) >> 24) as u32;
+		let new_prediction: u32 = (((bit_shift - old_prediction as i64) * multiplier) >> 32) as u32;
 		// update state
 		self.0 = self.0.wrapping_add((new_prediction & 0xFFFFFF00)
 			+ if count < 255 { 1 } else { 0 });
-		// return current prediction (before update)
-		return current_prediction as u64;
+		// return old prediction (before update)
+		return old_prediction;
 	}
 }
 
@@ -135,8 +135,8 @@ impl BitPrediction {
 //region BitEncoder
 
 struct BitEncoder<W: Write> {
-	low: u64,
-	high: u64,
+	low: u32,
+	high: u32,
 	states: Vec<BitPrediction>,
 	writer: W,
 }
@@ -145,7 +145,7 @@ impl<W: Write> BitEncoder<W> {
 	fn new(size: usize, writer: W) -> Self {
 		Self {
 			low: 0,
-			high: 0xFFFFFFFF_FFFFFFFF,
+			high: 0xFFFFFFFF,
 			states: vec![BitPrediction::new(); size],
 			writer,
 		}
@@ -157,18 +157,18 @@ impl<W: Write> BitEncoder<W> {
 		assert!(context < self.states.len());
 		assert!(bit == 0 || bit == 1);
 		// get prediction
-		let prediction: u64 = self.states[context].update(bit);
+		let prediction: u32 = self.states[context].update(bit);
 		// get delta
-		let delta: u64 = (((self.high - self.low) as u128 * prediction as u128) >> 24) as u64;
+		let delta: u32 = (((self.high - self.low) as u64 * prediction as u64) >> 32) as u32;
 		// calculate middle
-		let middle: u64 = self.low + delta;
+		let middle: u32 = self.low + delta;
 		assert!(self.low <= middle && middle < self.high);
 		// set new range limit
-		*(if bit != 0 { &mut self.high } else { &mut self.low }) = middle + (bit ^ 1) as u64;
+		*(if bit != 0 { &mut self.high } else { &mut self.low }) = middle + (bit ^ 1) as u32;
 		// shift bits out
-		while (self.high ^ self.low) & 0xFF000000_00000000 == 0 {
+		while (self.high ^ self.low) & 0xFF000000 == 0 {
 			// write byte
-			self.writer.write_all(&[(self.low >> 56) as u8])?;
+			self.writer.write_all(&[(self.low >> 24) as u8])?;
 			// shift new bits into high/low
 			self.low = self.low << 8;
 			self.high = (self.high << 8) | 0xFF;
@@ -199,7 +199,7 @@ impl<W: Write> BitEncoder<W> {
 
 	fn flush(mut self) -> AnyResult<W> {
 		// write then get out
-		self.writer.write_all(&[(self.low >> 56) as u8])?;
+		self.writer.write_all(&[(self.low >> 24) as u8])?;
 		// oke, give back the writer
 		Ok(self.writer)
 	}
@@ -210,9 +210,9 @@ impl<W: Write> BitEncoder<W> {
 //region BitDecoder
 
 struct BitDecoder<R: Read> {
-	value: u64,
-	low: u64,
-	high: u64,
+	value: u32,
+	low: u32,
+	high: u32,
 	states: Vec<BitPrediction>,
 	reader: R,
 }
@@ -230,12 +230,12 @@ impl<R: Read> BitDecoder<R> {
 
 	fn bit(&mut self, context: usize) -> AnyResult<usize> {
 		// shift bits in
-		while (self.high ^ self.low) & 0xFF000000_00000000 == 0 {
+		while (self.high ^ self.low) & 0xFF000000 == 0 {
 			// read byte
 			let mut byte: [u8; 1] = [0];
 			let read: usize = self.reader.read(&mut byte)?;
 			// shift new bits into high/low/value
-			self.value = (self.value << 8) | if read > 0 { byte[0] as u64 } else { 0xFF };
+			self.value = (self.value << 8) | if read > 0 { byte[0] as u32 } else { 0xFF };
 			self.low = self.low << 8;
 			self.high = (self.high << 8) | 0xFF;
 		}
@@ -244,16 +244,16 @@ impl<R: Read> BitDecoder<R> {
 		assert!(self.low < self.high);
 		// get prediction
 		let bit_prediction: &mut BitPrediction = &mut self.states[context];
-		let prediction: u64 = bit_prediction.get_prediction();
+		let prediction: u32 = bit_prediction.get_prediction();
 		// get delta
-		let delta: u64 = (((self.high - self.low) as u128 * prediction as u128) >> 24) as u64;
+		let delta: u32 = (((self.high - self.low) as u64 * prediction as u64) >> 32) as u32;
 		// calculate middle
-		let middle: u64 = self.low + delta;
+		let middle: u32 = self.low + delta;
 		assert!(self.low <= middle && middle < self.high);
 		// calculate bit
 		let bit: usize = if self.value <= middle { 1 } else { 0 };
 		// update high/low
-		*(if bit != 0 { &mut self.high } else { &mut self.low }) = middle + (bit ^ 1) as u64;
+		*(if bit != 0 { &mut self.high } else { &mut self.low }) = middle + (bit ^ 1) as u32;
 		// update bit prediction
 		bit_prediction.update(bit);
 		// return the value
@@ -400,7 +400,7 @@ impl<W: Write + Send + 'static> ThreadedEncoder<W> {
 				Err(_) => return Err(AnyError::new("Broken connection between threads!")),
 			};
 			// check if this is the end, exit if it is
-			if message.count > BUFFER_SIZE { break }
+			if message.count > BUFFER_SIZE { break; }
 			// encode every bit in buffer
 			let buffer: BufferGuarded = message.buffer.lock()?;
 			for i in 0..message.count {
